1985f14f1a02f5518e64f0fc53f7a3a268120fe4
Hariharan <hariharantlr2401@gmail.com>
2024-05-26 00:17:16 +0530

    firstcmmut

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state,setState]= useState('test@gmail.com');
+  const [inp,setinp]=useState('');
+  const [pass,setpass]=useState('12345678');
+  const [inp2,setinp2]=useState('');
+  const [effect,seteffect]=useState('False');
+  const einput=(event)=>{
+    setinp(event.target.value);
+  }
+  const pinput=(event)=>{
+     setinp2(event.target.value);
+  }
+  useEffect(()=>{
+    if(effect==='True'){
+        inp===state ? pass===inp2 ? alert("Login Successful") : alert("Incorrect Password"):alert("Incorrect Email");
+    }
+    seteffect('False');
+  },);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='App'>
+      <h1>LOGIN USING USEEFFECT</h1>
+    <label>Email ID :</label>
+    <input type='email'  placeholder='Enter emailID' onChange={einput}></input>
+    <br></br>
+    <label>Password :</label>
+    <input type='password'  placeholder='Enter Password' onChange={pinput}></input>
+    <br></br>
+    <button type='submit' onClick={()=>seteffect('True')}>LOGIN</button>
     </div>
   );
 }
